d0af2111d1060432204e9f9c57d6f6eaa4d600ff
rezuan98 <rezuanahmmeds@gmail.com>
2025-01-25 17:43:34 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./banner"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-5xl text-yellow-500 text-center'>WE ARE HERE TO LEARN REACT FROM TAPASCRIPT</h1>
+      <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Banner.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Banner() {
+  return (
+    <div><h3 className="bg-gray-100 p-4 text-center text-green-700 text-3xl ">Banner, This is a Banner Section</h3></div>
+  )
+}
