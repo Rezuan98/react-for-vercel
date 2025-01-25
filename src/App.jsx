d20a6787f3c4c43aa465cd2a08d4ca784b6fe4bb
rezuan98 <rezuanahmmeds@gmail.com>
2025-01-25 18:26:56 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import Banner from "./Banner"
 
 
 function App() {
-
-
   return (
     <>
-      <h1 className='text-5xl text-yellow-500 text-center'>WE ARE HERE TO LEARN REACT FROM TAPASCRIPT</h1>
-      <Banner/>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="w-[450px] bg-green-100 border-2 border-green-500 rounded-lg p-6 shadow-lg text-center">
+          <h1 className="text-3xl text-green-700 font-bold">
+            WE ARE HERE TO LEARN REACT FROM TAPASCRIPT
+          </h1>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
